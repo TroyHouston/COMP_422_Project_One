7b9f18f27a894c9c25c9db42a74e1536f13a6a39
Troy Houston <troy.houston@outlook.com>
2015-08-14 10:54:35 +1200

    Partial work on part 3

--- src/project/DataMining.java
+++ src/project/DataMining.java
@@ -1,5 +1,20 @@
 package project;
 
+import java.io.*;
+import java.nio.Buffer;
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+
+import weka.classifiers.Evaluation;
+import weka.classifiers.trees.J48;
+import weka.core.Instances;
+import weka.core.converters.ArffSaver;
+import weka.core.converters.CSVLoader;
+import weka.core.converters.ConverterUtils;
+import weka.core.converters.ConverterUtils.DataSource;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.NumericToNominal;
+
 /**
  * Created by troyr on 8/7/2015.
  */
@@ -27,6 +42,272 @@ public class DataMining {
     // Below? - Results - Confusion Matrix - Percentages.
     //
     // Needs to support Comparisons.
+    //
+    // Try a setting for using cross validation.
+
+    /**
+     * Probably can replace this with a temp stringBuilder now.
+     */
+
+    private J48 tree;
+    public String results;
+
+    public DataMining() {
+
+
+
+    }
+
+    public String getGraph() {
+        try {
+            return tree.graph();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    public void trainJ48Classifier() {
+
+        csvToArff("output\\trainingData.csv", "output\\trainingData.arff");
+        csvToArff("output\\testData.csv", "output\\testData.arff");
+
+        DataSource testData = null;
+        DataSource trainingData = null;
+        String[] options = new String[1];
+        // Unpruned tree
+        options[0] = "-U";
+        try {
+            testData = new DataSource("output\\testData.arff");
+            trainingData = new DataSource("output\\trainingData.arff");
+            Instances test = testData.getDataSet();
+            Instances train = trainingData.getDataSet();
+            test.setClassIndex(0);
+            train.setClassIndex(0);
+
+            NumericToNominal convert = new NumericToNominal();
+            String[] fOptions = new String[2];
+            fOptions[0] = "-R";
+            fOptions[1] = "first";  //range of variables to change
+
+            convert.setOptions(fOptions);
+            convert.setInputFormat(train);
+
+            train = Filter.useFilter(train, convert);
+            test = Filter.useFilter(test, convert);
+
+            tree = new J48();         // new instance of tree
+            tree.setOptions(options);     // set the options
+            tree.buildClassifier(train);   // build classifier
+
+            // evaluate classifier and print some statistics
+            Evaluation eval = new Evaluation(train);
+            eval.evaluateModel(tree, test);
+            results = eval.toSummaryString("\nResults\n======\n", false);
+            System.out.println(eval.toSummaryString("\nResults\n======\n", false));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private void csvToArff(String csv, String arff) {
+        // load CSV
+        CSVLoader loader = new CSVLoader();
+        ArffSaver saver = new ArffSaver();
+        try {
+            loader.setSource(new File(csv));
+            Instances data = null;
+            data = loader.getDataSet();
+
+
+            // save ARFF
+            saver.setInstances(data);
+            saver.setFile(new File(arff));
+            saver.writeBatch();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * Load all the data into and internal data array.
+     * Write all data to an external file.
+     *
+     * @param filesToLoad - Which files of the six should be loaded. 0 = No, 1 = Yes.
+     */
+    public void loadData(int filesToLoad[]) {
+
+        ArrayList<BufferedReader> readers = new ArrayList<BufferedReader>();
+        PrintWriter allWriter;
+        PrintWriter testWriter;
+        PrintWriter trainingWriter;
+
+        try {
+            createReader(filesToLoad[0], "fac", readers);
+            createReader(filesToLoad[1], "fou", readers);
+            createReader(filesToLoad[2], "kar", readers);
+            createReader(filesToLoad[3], "mor", readers);
+            createReader(filesToLoad[4], "pix", readers);
+            createReader(filesToLoad[5], "zer", readers);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        // Create blank file
+        File allData = new File("output\\allData.csv");
+        File testData = new File("output\\testData.csv");
+        File trainingData = new File("output\\trainingData.csv");
+        try {
+            allWriter = new PrintWriter(allData);
+            testWriter = new PrintWriter(testData);
+            trainingWriter = new PrintWriter(trainingData);
+
+            // Create data table
+            StringBuilder tempLine = new StringBuilder();
+            String currentLine = "";
+            String lineSplit[];
+            String csv = ",";
+            int index = 1;
+
+            currentLine = getAttributeLine(filesToLoad);
+
+            allWriter.append(currentLine);
+            testWriter.append(currentLine);
+            trainingWriter.append(currentLine);
+            allWriter.append("\n");
+            testWriter.append("\n");
+            trainingWriter.append("\n");
+
+            for (int i = 0; i < 2000; i++) {
+                // Get current class
+                String currentClass = String.valueOf(i / 200);
+                tempLine = new StringBuilder();
+                tempLine.append(currentClass);
+                tempLine.append(csv);
+                index = 0;
+
+                // Add data to internal data table
+                for (BufferedReader r : readers) {
+                    index++;
+                    currentLine = readers.get(index - 1).readLine();
+                    lineSplit = currentLine.trim().split("\\s+");
+                    for (String n: lineSplit) {
+                        tempLine.append(n);
+                        tempLine.append(csv);
+                    }
+                }
+                currentLine = tempLine.toString();
+                currentLine = currentLine.substring(0, currentLine.length()-1);
+
+                // Write to external tabular data set
+
+                allWriter.append(currentLine);
+                if (i % 2 == 0)
+                    testWriter.append(currentLine);
+                else if (i % 2 == 1)
+                    trainingWriter.append(currentLine);
+
+                allWriter.append("\n");
+                if (i % 2 == 0)
+                    testWriter.append("\n");
+                else if (i % 2 == 1)
+                    trainingWriter.append("\n");
+            }
+
+
+            // Close readers.
+            for (BufferedReader br : readers) {
+                br.close();
+            }
+            allWriter.close();
+            testWriter.close();
+            trainingWriter.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("File Not Found: Failed to create data file.");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("IO Exception: Failed to read & write.");
+        }
+
+
+    }
+
+    /**
+     * Create the first line of the respective csv data files.
+     * i.e Containing the headings.
+     *
+     * @param files - Which datasets are being used.
+     * @return
+     */
+    private String getAttributeLine(int[] files) {
+        String csv = ",";
+        StringBuilder line = new StringBuilder();
+        String finalLine;
+
+        line.append("Class");
+        line.append(csv);
+
+        if (files[0] == 1) {
+            for (int i = 1; i <= 216; i++) {
+                line.append("fac" + i);
+                line.append(csv);
+            }
+        }
+
+        if (files[1] == 1) {
+            for (int i = 1; i <= 76; i++) {
+                line.append("fou" + i);
+                line.append(csv);
+            }
+        }
+
+        if (files[2] == 1) {
+            for (int i = 1; i <= 64; i++) {
+                line.append("kar" + i);
+                line.append(csv);
+            }
+        }
+
+        if (files[3] == 1) {
+            for (int i = 1; i <= 6; i++) {
+                line.append("mor" + i);
+                line.append(csv);
+            }
+        }
+
+        if (files[4] == 1) {
+            for (int i = 1; i <= 240; i++) {
+                line.append("pix" + i);
+                line.append(csv);
+            }
+        }
+
+        if (files[5] == 1) {
+            for (int i = 1; i <= 47; i++) {
+                line.append("zer" + i);
+                line.append(csv);
+            }
+        }
+        finalLine = line.toString();
+        finalLine = finalLine.substring(0, finalLine.length()-1);
+        return  finalLine;
+    }
+
+    /**
+     * If this file should be loaded. Create buffered reader and add to global array of file readers.
+     */
+    private void createReader(int bool, String file, ArrayList<BufferedReader> readers) throws FileNotFoundException {
+        if (bool == 1)
+            readers.add(new BufferedReader(new FileReader("assets\\mfeat-digits\\mfeat-" + file)));
+    }
 
 
 
--- src/project/TabFour.java
+++ src/project/TabFour.java
@@ -1,27 +1,36 @@
 package project;
 
+import javafx.collections.ObservableList;
+import javafx.embed.swing.SwingNode;
 import javafx.geometry.Insets;
-import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
-import javafx.scene.control.ScrollBar;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import weka.gui.treevisualizer.PlaceNode2;
+import weka.gui.treevisualizer.TreeVisualizer;
+import javafx.scene.control.TextArea;
+
+import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by troyr on 8/7/2015.
  */
 public class TabFour extends GridPane {
 
-    DataMining main;
+    private DataMining main;
 
     public TabFour(Stage primaryStage) {
 
         main = new DataMining();
+        int filesToLoad[] = {1,1,1,1,1,1};
+        main.loadData(filesToLoad);
+        main.trainJ48Classifier();
 
         // Text/Title
         // Button one --- button two
@@ -57,17 +66,45 @@ public class TabFour extends GridPane {
         hTitle.setAlignment(Pos.CENTER_LEFT);
         this.add(hTitle, 0, 0);
 
-        // Table
-        TableView dataTable1 = new TableView();
-        dataTable1.setEditable(false);
-        //ScrollBar hScroll = findScrollBar( dataTable1, Orientation.HORIZONTAL);
-        //ScrollBar vSCroll = findScrollBar( dataTable1, Orientation.VERTICAL);
+        TextArea results = new TextArea();
+        results.setFont(new Font("Courier New", 14));
+results.setPrefSize(600, 250);
+        results.setText(main.results);
 
-        TableColumn firstNameCol = new TableColumn("Class ID");
-        TableColumn lastNameCol = new TableColumn("Feature 1");
-        TableColumn emailCol = new TableColumn("Feature 2");
+        TextArea results2 = new TextArea();
+        results2.setFont(new Font("Courier New", 14));
+        results2.setPrefSize(600, 250);
+        results2.setText(main.results);
 
-        dataTable1.getColumns().addAll(firstNameCol, lastNameCol, emailCol);
-        this.add(dataTable1,0,1);
+        this.add(results, 0, 1);
+        this.add(results2,1,1);
+
+        createSwingWindow();
+
+
     }
+
+    /**
+     * Have a button to open this window....
+     */
+    private void createSwingWindow() {
+        // display classifier
+        final javax.swing.JFrame jf = new javax.swing.JFrame("Weka Classifier Tree Visualizer: J48");
+        jf.setSize(900,900);
+        jf.getContentPane().setLayout(new BorderLayout());
+        TreeVisualizer tv = new TreeVisualizer(null, main.getGraph(), new PlaceNode2());
+        tv.setDoubleBuffered(true);
+        // Overload the jf.paintcomponent method to repaint a stored treevis on resize.
+        jf.getContentPane().add(tv, BorderLayout.CENTER);
+        jf.addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowClosing(java.awt.event.WindowEvent e) {
+                jf.dispose();
+            }
+        });
+
+        jf.setVisible(true);
+        tv.fitToScreen();
+
+    }
+
 }
